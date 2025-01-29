aabdd2877de0fd9b2fd4cf10d2ab6f31bcbea64d
Andreas K <totonga@gmail.com>
2025-01-29 13:10:28 +0000

    examine performance

--- test/test_example_files.py
+++ test/test_example_files.py
@@ -144,3 +144,115 @@ class TestExampleFiles(unittest.TestCase):
                 failed = True
 
         self.assertFalse(failed, "At least one file failed")
+
+    def test_files_channel_by_chanel(self):
+        """test loops over all files and checks if values do match info in structure"""
+        example_files_folder = pathlib.Path.joinpath(pathlib.Path(
+            __file__).parent.resolve(), "..", "data")
+        example_files = [y for x in os.walk(
+            example_files_folder) for y in glob(os.path.join(x[0], "*.dxd"))]
+
+        example_files2 = [y for x in os.walk(
+            example_files_folder) for y in glob(os.path.join(x[0], "*.d7d"))]
+        for file in example_files2:
+            example_files.append(file)
+
+        example_files2 = [y for x in os.walk(
+            example_files_folder) for y in glob(os.path.join(x[0], "*.d7z"))]
+        for file in example_files2:
+            example_files.append(file)
+
+        failed = False
+        for example_file in example_files:
+            example_file_uri = pathlib.Path(
+                example_file).absolute().resolve().as_uri()
+            self.log.info("URI: %s", example_file_uri)
+
+            try:
+                self.log.info("Retrieve structure")
+                structure = self.__load_structure(example_file_uri)
+                self.assertNotEqual(structure.name, "")
+                self.assertNotEqual(structure.identifier.url, "")
+
+                self.log.info("Check bulk load")
+                handle = self.service.Open(oed.Identifier(
+                    url=example_file_uri, parameters=""), None)
+                try:
+                    for group in structure.groups:
+                        for values_channel_index, channel in enumerate(group.channels):
+
+                            values_channel = self.service.GetValues(
+                                oed.ValuesRequest(
+                                    handle=handle,
+                                    group_id=group.id,
+                                    start=0,
+                                    limit=group.number_of_rows + 10,
+                                    channel_ids=[channel.id],
+                                ),
+                                self.mock_context,
+                            ).channels[0]
+
+                            structure_channel = group.channels[values_channel_index]
+                            self.assertEqual(
+                                values_channel.id, structure_channel.id)
+                            self.assertEqual(
+                                values_channel.values.data_type, structure_channel.data_type)
+                            if ods.DataTypeEnum.DT_COMPLEX == values_channel.values.data_type:
+                                vals = values_channel.values.float_array.values
+                                self.assertEqual(
+                                    len(vals), group.number_of_rows * 2)
+                            elif ods.DataTypeEnum.DT_DCOMPLEX == values_channel.values.data_type:
+                                vals = values_channel.values.double_array.values
+                                self.assertEqual(
+                                    len(vals), group.number_of_rows * 2)
+                            elif ods.DataTypeEnum.DT_BYTE == values_channel.values.data_type:
+                                vals = values_channel.values.byte_array.values
+                                self.assertEqual(
+                                    len(vals), group.number_of_rows)
+                            elif ods.DataTypeEnum.DT_SHORT == values_channel.values.data_type:
+                                vals = values_channel.values.long_array.values
+                                self.assertEqual(
+                                    len(vals), group.number_of_rows)
+                            elif ods.DataTypeEnum.DT_LONG == values_channel.values.data_type:
+                                vals = values_channel.values.long_array.values
+                                self.assertEqual(
+                                    len(vals), group.number_of_rows)
+                            elif ods.DataTypeEnum.DT_LONGLONG == values_channel.values.data_type:
+                                vals = values_channel.values.longlong_array.values
+                                self.assertEqual(
+                                    len(vals), group.number_of_rows)
+                            elif ods.DataTypeEnum.DT_FLOAT == values_channel.values.data_type:
+                                vals = values_channel.values.float_array.values
+                                self.assertEqual(
+                                    len(vals), group.number_of_rows)
+                            elif ods.DataTypeEnum.DT_DOUBLE == values_channel.values.data_type:
+                                vals = values_channel.values.double_array.values
+                                self.assertEqual(
+                                    len(vals), group.number_of_rows)
+                            elif ods.DataTypeEnum.DT_STRING == values_channel.values.data_type:
+                                vals = values_channel.values.string_array.values
+                                self.assertEqual(
+                                    len(vals), group.number_of_rows)
+                            elif ods.DataTypeEnum.DT_DATE == values_channel.values.data_type:
+                                vals = values_channel.values.string_array.values
+                                self.assertEqual(
+                                    len(vals), group.number_of_rows)
+                            elif ods.DataTypeEnum.DT_BYTESTR == values_channel.values.data_type:
+                                vals = values_channel.values.bytestr_array.values
+                                self.assertEqual(
+                                    len(vals), group.number_of_rows)
+                            elif ods.DataTypeEnum.DT_BOOLEAN == values_channel.values.data_type:
+                                vals = values_channel.values.boolean_array.values
+                                self.assertEqual(
+                                    len(vals), group.number_of_rows)
+                            else:
+                                raise ValueError(
+                                    f"Unknown type {values_channel.values.data_type}")
+
+                finally:
+                    self.service.Close(handle, None)
+            except Exception as e:
+                self.log.error(f"FAILED: {example_file}: {e}")
+                failed = True
+
+        self.assertFalse(failed, "At least one file failed")
